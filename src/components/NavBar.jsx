c544db516d615d93f6634fc89c544dfabef86428
josh-alhassan <saintjosh26@gmail.com>
2023-11-29 22:14:51 +0100

    route logo to homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './NavBar.css';
 import NavItem from './NavItem';
+import { Link } from 'react-router-dom'
 // import Booking from '../pages/Booking';
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function NavBar() {
   return (
     <div className='nav__section'>
-      <h3 className="logo">Sunflawanroses</h3>
+      <Link to='/'> <h3 className="logo" url='/' >Sunflawanroses</h3> </Link>
 
       <nav>
         <ul className="nav__items">
